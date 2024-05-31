34208e5662771b7b523bd32b5a5eb31d9da1bbb6
JaiPannu-IITI <pannujai06iiti@icloud.com>
2024-06-01 01:27:16 +0530

    i frame fixes

--- src/components/homepage/footer.tsx
+++ src/components/homepage/footer.tsx
@@ -40,7 +40,7 @@ export default function Footer(props: any) {
 
             </div>
 
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3685.330526493356!2d75.92317729999999!3d22.529287999999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3962efb7df4ad60b%3A0xb51ea33269875e24!2sIIT%20Indore%20Simrol%20Madhya%20Pradesh!5e0!3m2!1sen!2sin!4v1717103018656!5m2!1sen!2sin" width="340" height="260" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" className={styles.googlemap}></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3685.330526493356!2d75.92317729999999!3d22.529287999999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3962efb7df4ad60b%3A0xb51ea33269875e24!2sIIT%20Indore%20Simrol%20Madhya%20Pradesh!5e0!3m2!1sen!2sin!4v1717103018656!5m2!1sen!2sin" width="340" height="260"  loading="lazy"  className={styles.googlemap}></iframe>
 
 
           </div>
