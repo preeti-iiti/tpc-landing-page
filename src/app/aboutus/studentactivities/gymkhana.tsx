0a051282acb1e5955d9e11e48db4b56d9887a303
JaiPannu-IITI <pannujai06iiti@icloud.com>
2024-06-14 23:19:27 +0530

    offwhite

--- src/app/aboutus/studentactivities/gymkhana.tsx
+++ src/app/aboutus/studentactivities/gymkhana.tsx
@@ -40,16 +40,8 @@ export default function Gymkhana() {
                 ))}
             </section>
 
-        <div>
-            <h1 className={styles.H1}> Students Gymkhana</h1>
-            <p>
-            The Student Gymkhana at IIT Indore is led by the Senate, consisting of executives and councillors. Elected representatives focus on fostering student development and leadership through coordinating events and maximizing individual potential. Numerous clubs and groups cater to diverse co-curricular interests.
-            </p>
-        </div>
-        <h2>Tech clubs</h2>
-        <section className={styles.table}>
-            <div><img src="/data/logo/iiti.png" alt="" /></div>
-        </section>
+       
+        
 
         </div>
     );
