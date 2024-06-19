6820c8be7d5435a34c069a5450dd13d06e963b15
JaiPannu-IITI <pannujai06iiti@icloud.com>
2024-06-20 03:16:21 +0530

    activity card update

--- src/app/aboutus/studentactivities/studentactivities.tsx
+++ src/app/aboutus/studentactivities/studentactivities.tsx
@@ -11,17 +11,18 @@ export default function Activities() {
             <div className={styles.cardContainer}>
                 {data.activities.map((activity, index) => (
                     <div key={index} className={styles.card}>
-                       
-                        <Image   src={activity.img} alt={activity.title} className={styles.image}  />
-                   
-                        {/* <h3 className={styles.title}>{activity.title}</h3> */}
-                   
+                    <div className={`${styles.cardside} ${styles.front}`}>
+                        <Image src={activity.img} alt={activity.title} className={styles.image}  />
+                   </div>
+                      
+                        <div className={`${styles.cardside} ${styles.back}`}>
                         <Link href={activity.link} target='_blank'>
                         <div className={`para text-[0.8rem] ${styles.overlay} text-white`}><div className=' font-bold text-[1rem]'>{activity.title}</div>{activity.description}
                         
                         <Image src={data.insta} alt="insta" width={40} className=' brightness-0 invert-[1] relative left-[150px]' />
                          </div>
                          </Link>
+                         </div>
                     </div>
                 ))}
             </div>
