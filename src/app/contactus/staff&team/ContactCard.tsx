a6d8b7d21bc6d733df72956850a6b497d40dd7cf
JaiPannu-IITI <pannujai06iiti@icloud.com>
2024-08-08 12:11:32 +0530

    cards fxx

--- src/app/contactus/staff&team/ContactCard.tsx
+++ src/app/contactus/staff&team/ContactCard.tsx
@@ -1,48 +1,42 @@
 "use client";
-import React from 'react';
-import styles from './ContactCard.module.css';
-import info from './Info.json';
-import {Image} from "@nextui-org/image";
-import Link from 'next/link';
+import React from "react";
+import styles from "./ContactCard.module.css";
+import info from "./Info.json";
+import { Image } from "@nextui-org/image";
+import Link from "next/link";
 
 const ContactCard: React.FC = () => {
-    console.log("Info data: ", info.Info); // Add this line
+  console.log("Info data: ", info.Info); // Add this line
 
-    return (
-        <div className={styles.cardContainer}>
-            {info.Info.map((person, index) => (
-                <div key={index} className={styles.card}>
-                    <div className={`${styles.cardside} ${styles.front}`}>
-                     
-                    <div className={styles.imageContainer}>
-                        <Image src="/data/logo/iiti.png" alt={person.name} className={styles.profileImage} />
-                        
-                    </div>
-                    <div className={styles.info}>
-                        <h2>{person.name}</h2>
-                        <p>{person.position}</p>
-                        <p>{person.department}</p>
-                        <p>Email: <a href={`mailto:${person.email}`}>{person.email}</a></p>
-                        <p>Phone: {person.phone} (Extn. {person.extension})</p>
-                        {person.mobile && <p>Mobile: {person.mobile}</p>}
-                    </div>
-                    </div>
-                    <div className={`${styles.cardside} ${styles.back}`}>
-                    <div className=" flex flex-col w-[100%] h-[100%] justify-around items-center">
-                        <Image src="/data/logo/iiti.png" alt={person.name} width={250} className={styles.embeded} />
-                        <div className={styles.socialContainer}>
-                            {Object.entries(person.social).map(([platform, url]) => (
-                                <a href={url} key={platform} className={styles[platform]} target="_blank" rel="noopener noreferrer">
-                                    <i className={`fab fa-${platform}`}></i>
-                                </a>
-                            ))}
-                        </div>
-                    </div>
-                    </div>
-                </div>
-            ))}
+  return (
+    <div className={styles.cardContainer}>
+      {info.Info.map((person, index) => (
+        <div key={index} className={styles.card}>
+          <div className={`${styles.cardside} `}>
+            <div className={styles.imageContainer}>
+              <Image
+                src="/data/logo/iiti.png"
+                alt={person.name}
+                className={styles.profileImage}
+              />
+            </div>
+            <div className={styles.info}>
+              <h2>{person.name}</h2>
+              <p>{person.position}</p>
+              <p>{person.department}</p>
+              <p>
+                Email: <a href={`mailto:${person.email}`}>{person.email}</a>
+              </p>
+              <p>
+                Phone: {person.phone} (Extn. {person.extension})
+              </p>
+              {person.mobile && <p>Mobile: {person.mobile}</p>}
+            </div>
+          </div>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default ContactCard;
--- src/app/contactus/staff&team/page.tsx
+++ src/app/contactus/staff&team/page.tsx
@@ -1,57 +1,63 @@
-import React from 'react';
+import React from "react";
 import { Button } from "@nextui-org/react";
 import data from "./Info.json";
 import FacultyProfile from "./FacultyProfile";
 import FacultyProfile2 from "./pgteam";
 import styles from "./FacultyProfile.module.css";
+import ContactCard from "./ContactCard";
 
 const StaffTeam: React.FC = () => {
   return (
     <div className="flex flex-col gap-[35px]">
       <div className="questitle">tpc staff</div>
+      <ContactCard></ContactCard>
       <div className="questitle">placement team</div>
       <div className="header3">undergraduate</div>
 
       <Button color="danger" variant="light" size="lg">
-        <div className={`para my-3 text-wrap`}>Computer Science and Engineering</div>
+        <div className={`para my-3 text-wrap`}>
+          Computer Science and Engineering
+        </div>
       </Button>
       <div className="flex flex-wrap gap-10 justify-between">
-      {data.teamug[0].map((member: any, index: number) => (
-        <FacultyProfile key={index} {...member} />
-      ))}
-</div>
+        {data.teamug[0].map((member: any, index: number) => (
+          <FacultyProfile key={index} {...member} />
+        ))}
+      </div>
       <Button color="danger" variant="light" size="lg">
         <div className={`para my-3 text-wrap`}>Electrical Engineering</div>
       </Button>
       <div className="flex flex-wrap gap-10 justify-between">
-      {data.teamug[1].map((member: any, index: number) => (
-        <FacultyProfile key={index} {...member} />
-      ))}
-</div>
+        {data.teamug[1].map((member: any, index: number) => (
+          <FacultyProfile key={index} {...member} />
+        ))}
+      </div>
       <Button color="danger" variant="light" size="lg">
         <div className={`para my-3 text-wrap`}>Mechanical Engineering</div>
       </Button>
       <div className="flex flex-wrap gap-10 justify-between">
-      {data.teamug[2].map((member: any, index: number) => (
-        <FacultyProfile key={index} {...member} />
-      ))}
-</div>
+        {data.teamug[2].map((member: any, index: number) => (
+          <FacultyProfile key={index} {...member} />
+        ))}
+      </div>
       <Button color="danger" variant="light" size="lg">
-        <div className={`para my-3 text-wrap`}>Metallurgical Engineering and Materials Science</div>
+        <div className={`para my-3 text-wrap`}>
+          Metallurgical Engineering and Materials Science
+        </div>
       </Button>
       <div className="flex flex-wrap gap-10 justify-between">
-      {data.teamug[3].map((member: any, index: number) => (
-        <FacultyProfile key={index} {...member} />
-      ))}
-</div>
+        {data.teamug[3].map((member: any, index: number) => (
+          <FacultyProfile key={index} {...member} />
+        ))}
+      </div>
       <Button color="danger" variant="light" size="lg">
         <div className={`para my-3 text-wrap`}>Civil Engineering</div>
       </Button>
       <div className="flex flex-wrap gap-10 justify-between">
-      {data.teamug[4].map((member: any, index: number) => (
-        <FacultyProfile key={index} {...member} />
-      ))}
-</div>
+        {data.teamug[4].map((member: any, index: number) => (
+          <FacultyProfile key={index} {...member} />
+        ))}
+      </div>
       <div className="header3">postgraduate</div>
       <div className={styles.cardContainer}>
         {data.teampg.map((item: any, index: number) => (
