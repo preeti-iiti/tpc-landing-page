79375e5c026c4129e8ddb72f18ae390ff1a644bb
JaiPannu-IITI <pannujai06iiti@icloud.com>
2024-08-08 17:22:51 +0530

    messages brought up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,18 @@
+import Carousel from "./components/carousel/carousel";
+import PortalForm from "./components/portalform/portalform";
+import Recruiters from "./components/recruiters/recruiters";
+import Messages from "./components/messages/messages";
 
-import Carousel from "./components/carousel/carousel"
-import PortalForm from "./components/portalform/portalform"
-import Recruiters from "./components/recruiters/recruiters"
-import Messages from "./components/messages/messages"
-
-import CarouselData from "./components/carousel/carousel.json"
-import RecruitersData from "./components/recruiters/ourrecruiters.json"
+import CarouselData from "./components/carousel/carousel.json";
+import RecruitersData from "./components/recruiters/ourrecruiters.json";
 
 export default function Home() {
   return (
-   <>
-   
-
-   <Carousel {...CarouselData}/>
-   <PortalForm />
-   <Recruiters {...RecruitersData}/>
-   <Messages />
-
-
- 
-   </>
-    
+    <>
+      <Carousel {...CarouselData} />
+      <PortalForm />
+      <Messages />
+      <Recruiters {...RecruitersData} />
+    </>
   );
 }
