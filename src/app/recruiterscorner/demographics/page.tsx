898c900451005f369b358d2a76e7f1f827a05ef8
JaiPannu-IITI <pannujai06iiti@icloud.com>
2024-08-08 13:17:12 +0530

    demographics

--- src/app/recruiterscorner/demographics/page.tsx
+++ src/app/recruiterscorner/demographics/page.tsx
@@ -1,45 +1,48 @@
-import React from 'react';
-import PieChart from './chart';
-import { ChartData } from 'chart.js';
-import styles from './page.module.css';
-import departmentData from './data.json';
+import React from "react";
+import PieChart from "./chart";
+import { ChartData } from "chart.js";
+import styles from "./page.module.css";
+import departmentData from "./data.json";
 import { Button } from "@nextui-org/react";
 import Image from "next/image";
 import Link from "next/link";
 
-const createChartData = (labels: string[], data: number[]): ChartData<'pie'> => {
+const createChartData = (
+  labels: string[],
+  data: number[]
+): ChartData<"pie"> => {
   return {
     labels,
     datasets: [
       {
-        label: 'Number of Students',
+        label: "Number of Students",
         data,
         backgroundColor: [
-          'rgba(255, 99, 132, 0.6)', // Soft Red
-          'rgba(54, 162, 235, 0.6)', // Soft Blue
-          'rgba(255, 206, 86, 0.6)', // Soft Yellow
-          'rgba(75, 192, 192, 0.6)', // Soft Green
-          'rgba(153, 102, 255, 0.6)', // Soft Purple
-          'rgba(255, 159, 64, 0.6)', // Soft Orange
+          "rgba(102, 102, 255, 1)", // Soft Yellow
+          "rgba(0, 204, 204,1)", // Soft Red
+          "rgba(51, 255, 255, 1)", // Soft Blue
+          "rgba(75, 192, 192, 0.6)", // Soft Green
+          "rgba(153, 102, 255, 0.6)", // Soft Purple
+          "rgba(255, 159, 64, 0.6)", // Soft Orange
         ],
-        
+
         borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)',
+          "rgba(255, 255, 255, 1)",
+          "rgba(255, 255, 255, 1)",
+          "rgba(255, 255, 255, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
         ],
         borderWidth: 1,
         hoverBorderWidth: 4,
         hoverBorderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)',
+          "rgba(102, 102, 255, 1)",
+          "rgba(0, 204, 204, 1)",
+          "rgba(51, 255, 255, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
         ],
       },
     ],
@@ -49,27 +52,43 @@ const createChartData = (labels: string[], data: number[]): ChartData<'pie'> => 
 const Demographics: React.FC = () => {
   return (
     <div className={styles.wrapper}>
-      <div className='header1'>Demographics</div>
+      <div className="header1">Demographics</div>
       <div className={styles.container}>
         {departmentData.departmentData.map((dept, index) => (
           <div key={index} className={styles.department}>
-            <div className=' flex justify-between items-center'>
-            <h2 className=' questitle'>{dept.departmentName}</h2>
-            <div className=" flex ">
-          <Link href="/aboutus/achievements">
-            <Button isIconOnly  className={` flex justify-center m-[5px] bg-transparent h-[3rem] w-[2rem] `}>
-              <Image src="/data/buttonspngs/medal.png" width={30} height={30} alt="achievements"/>
-            </Button>
-          </Link>
-        </div>
-        </div>
-            <PieChart data={createChartData(dept.pieLabels.labels, dept.pieLabels.data)} />
-            <ul>
+            <div className=" flex justify-between items-center">
+              <h2 className=" questitle">{dept.departmentName}</h2>
+              <div className=" flex ">
+                {/* <Link href="/aboutus/achievements">
+                  <Button
+                    isIconOnly
+                    className={` flex justify-center m-[5px] bg-transparent h-[3rem] w-[2rem] `}
+                  >
+                    <Image
+                      src="/data/buttonspngs/medal.png"
+                      width={30}
+                      height={30}
+                      alt="achievements"
+                    />
+                  </Button>
+                </Link> */}
+              </div>
+            </div>
+            <div className=" scale-85  w-[450px] h-[500px]">
+              <PieChart
+                data={createChartData(
+                  dept.pieLabels.labels,
+                  dept.pieLabels.data
+                )}
+              />
+            </div>
+            <div className=" p-3  text-center">
               {dept.labels.map((label, idx) => (
-                <li key={idx}><span>{label}</span>: {dept.data[idx]}</li>
+                <div key={idx}>
+                  <span>{label}</span>: {dept.data[idx]}
+                </div>
               ))}
-            </ul>
-            
+            </div>
           </div>
         ))}
       </div>
