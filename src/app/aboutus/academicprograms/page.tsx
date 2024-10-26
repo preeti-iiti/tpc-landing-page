cf67f61883f854d620883256ee6724e0fac08dea
Jai Pannu <142983705+JaiPannu-IITI@users.noreply.github.com>
2024-10-27 03:31:47 +0530

    jnwed

--- src/app/aboutus/academicprograms/page.tsx
+++ src/app/aboutus/academicprograms/page.tsx
@@ -68,11 +68,106 @@ export default function AcademicPrograms() {
 
   return (
     <>
-      <div className=" flex flex-col gap-[20px] items-start ">
+      <div className=" flex flex-col gap-[20px] items-center ">
         <div className=" flex justify-between w-[100%] ">
           <div className="header2">Academic Programs</div>
-          
-        </div>
+          </div>
+
+          <div className="overflow-x-auto">
+  <table className="min-w-full bg-white border border-gray-300">
+    <thead>
+      <tr>
+        <th className="px-4 py-2 border border-gray-300 text-gray-700 text-left font-semibold">B. Tech</th>
+        <th className="px-4 py-2 border border-gray-300 text-gray-700 text-left font-semibold">M. Tech</th>
+        <th className="px-4 py-2 border border-gray-300 text-gray-700 text-left font-semibold">MSc</th>
+        <th className="px-4 py-2 border border-gray-300 text-gray-700 text-left font-semibold">M.S (Research)</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td className="px-4 py-2 border border-gray-300"  rowSpan={2}>Civil Engineering</td>
+        <td className="px-4 py-2 border border-gray-300">Water, Climate and Sustainability</td>
+        <td className="px-4 py-2 border border-gray-300" >Chemistry</td>
+        <td className="px-4 py-2 border border-gray-300" >Computer Science & Engineering</td>
+      </tr>
+      <tr>
+        <td className="px-4 py-2 border border-gray-300">Structural Engineering</td>
+        <td className="px-4 py-2 border border-gray-300" >Physics</td>
+        <td className="px-4 py-2 border border-gray-300" >Electrical Engineering</td>
+      </tr>
+      <tr>
+        <td className="px-4 py-2 border border-gray-300" rowSpan={4}>Mechanical Engineering</td>
+        <td className="px-4 py-2 border border-gray-300">Advanced Manufacturing</td>
+        <td className="px-4 py-2 border border-gray-300" >Mathematics</td>
+        <td className="px-4 py-2 border border-gray-300" >Mechanical Engineering</td>
+      </tr>
+      <tr>
+        <td className="px-4 py-2 border border-gray-300">Mechanical System Design</td>
+        <td className="px-4 py-2 border border-gray-300" >Biotechnology</td>
+        <td className="px-4 py-2 border border-gray-300" >Space Science and Engineering</td>
+      </tr>
+      <tr>
+        <td className="px-4 py-2 border border-gray-300">Applied Optics and Laser Technology</td>
+        <td className="px-4 py-2 border border-gray-300" >Astronomy</td>
+        <td className="px-4 py-2 border border-gray-300" >Data Science and Management</td>
+      </tr>
+      <tr>
+        <td className="px-4 py-2 border border-gray-300">Thermal Energy Systems</td>
+        <td className="px-4 py-2 border border-gray-300"></td>
+        <td className="px-4 py-2 border border-gray-300" >Humanities and Social Science</td>
+      </tr>
+      <tr>
+        <td className="px-4 py-2 border border-gray-300"  rowSpan={2}>Metallurgical Engineering & Materials Science</td>
+        <td className="px-4 py-2 border border-gray-300" >Metallurgical Engineering</td>
+        <td className="px-4 py-2 border border-gray-300" ></td>
+        <td className="px-4 py-2 border border-gray-300" ></td>
+      </tr>
+      <tr>
+        <td className="px-4 py-2 border border-gray-300" >Materials Science and Engineering</td>
+        <td className="px-4 py-2 border border-gray-300" ></td>
+        <td className="px-4 py-2 border border-gray-300" ></td>
+      </tr>
+      <tr>
+        <td className="px-4 py-2 border border-gray-300" rowSpan={3}>Electrical Engineering</td>
+        <td className="px-4 py-2 border border-gray-300">Communication and Signal Processing</td>
+        <td className="px-4 py-2 border border-gray-300" ></td>
+        <td className="px-4 py-2 border border-gray-300" ></td>
+     
+      </tr>
+      <tr>
+        <td className="px-4 py-2 border border-gray-300">VLSI Design and Nanoelectronics</td>
+        <td className="px-4 py-2 border border-gray-300" ></td>
+        <td className="px-4 py-2 border border-gray-300" ></td>
+      </tr>
+      <tr>
+        <td className="px-4 py-2 border border-gray-300">Electrical Vehicles Intelligent Transport System</td>
+        <td className="px-4 py-2 border border-gray-300" ></td>
+        <td className="px-4 py-2 border border-gray-300" ></td>
+     
+      </tr>
+      <tr>
+        <td className="px-4 py-2 border border-gray-300" rowSpan={3}>Computer Science & Engineering</td>
+        <td className="px-4 py-2 border border-gray-300">Computer Science & Engineering</td>
+        <td className="px-4 py-2 border border-gray-300"></td>
+        <td className="px-4 py-2 border border-gray-300"></td>
+      </tr>
+     
+      <tr>
+        <td className="px-4 py-2 border border-gray-300">Space Engineering</td>
+        <td className="px-4 py-2 border border-gray-300"></td>
+        <td className="px-4 py-2 border border-gray-300"></td>
+      </tr>
+      <tr>
+    
+        <td className="px-4 py-2 border border-gray-300">Biomedical Engineering</td>
+        <td className="px-4 py-2 border border-gray-300"></td>
+        <td className="px-4 py-2 border border-gray-300"></td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+
+        
       </div>
     </>
   );
