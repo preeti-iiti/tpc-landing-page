e5cf7c3952099e594fd5068dddff4df8be3fe459
samnotfound404 <cse230001070@iiti.ac.in>
2024-10-27 00:19:30 +0530

    timeline

--- src/app/recruiterscorner/timelines/page.tsx
+++ src/app/recruiterscorner/timelines/page.tsx
@@ -1,4 +1,4 @@
-
+import { Timeline } from "@/components/ui/timeline";
 
 
 
@@ -8,10 +8,47 @@
 export default function Timelines() {
     return (
      <>
-     
-  
-    <h1>Timelines</h1>
-  
+
+   
+    <div className="min-h-screen bg-gray-100 dark:bg-gray-900">
+      <Timeline
+        data={[
+          {
+            "title": "Company Invitations",
+            "content": "The Training & Placement Cell (TPC), IIT Indore sends invitations to companies along with relevant information and Job Announcement Form (JAF). The recruiters also can send an email expressing their interest to participate in the campus recruitment program of IIT Indore on placementofficer@iiti.ac.in / apo@iiti.ac.in."
+          },
+          {
+            "title": "Job Announcement Form Submission",
+            "content": "Companies interested in recruiting can fill the JAF for each profile they wish to hire for."
+          },
+          {
+            "title": "Company Registration",
+            "content": "Once the filled JAF with all required details is received, companies are considered to be registered and will be contacted for further processes."
+          },
+          {
+            "title": "Pre-placement Talks",
+            "content": "Companies interested in conducting a pre-placement talk can send a request along with the preferred date(s)."
+          },
+          {
+            "title": "Job Announcements to Students",
+            "content": "The Job Announcement Form of the company will be made available online to all the eligible students. Interested students may apply online to each of the job roles offered by the company within a specified period."
+          },
+          {
+            "title": "Student Shortlisting",
+            "content": "The company can shortlist the students based on their shortlisting criteria like CV screening/assessment/GD."
+          },
+          {
+            "title": "Campus Interview Scheduling",
+            "content": "Further, the TPC office allots the dates for campus interviews considering factors like student preference, job profile, compensation, association with the institution, etc."
+          },
+          {
+            "title": "Final Selection List Submission",
+            "content": "The company is required to submit the list of selected and waitlisted (if any) candidates to the TPC office on the same day itself."
+          }
+        ]
+        }
+      />
+    </div>
    
      </>
       
