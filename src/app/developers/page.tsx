ab7d833587bfff1ed2e75a2a5d51df48186483c8
Prince Kumar <x.prince.x@outlook.com>
2025-06-25 13:26:29 +0530

    Developers - Add Website (#82)

--- src/app/developers/page.tsx
+++ src/app/developers/page.tsx
@@ -1,26 +1,48 @@
-import React from 'react'
-import data from './dev.json'
-import Image from 'next/image' 
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import EmailIcon from '@mui/icons-material/Email';
-import Link from 'next/link'; 
+import React from "react";
+import data from "./dev.json";
+import Image from "next/image";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import EmailIcon from "@mui/icons-material/Email";
+import Link from "next/link";
+import LanguageIcon from "@mui/icons-material/Language";
+
 export default function Page() {
   return (
-    <div className=' mb-8 flex flex-wrap justify-start gap-10'>
+    <div className=" mb-8 flex flex-wrap justify-start gap-10">
       {data.map((value, index) => (
-        <div key={index} className='relative shadow-lg w-[400px] rounded-xl overflow-clip  h-[500px] flex-col justify-between mx-auto '>
-          <div className='overflow-clip'><Image src={value.image} blurDataURL={value.image} alt="Profile Pic" width={500} height={500} /></div>
-          <div className=' flex justify-between p-4 items-center bg-white absolute bottom-0 z-10 w-full'>
-          <span className=' text-[1.5rem]  '>{value.name}</span>
-            <div className='flex gap-4'>
-            <Link href={value.linkedin} target='_blank'><LinkedInIcon/></Link>
-            <Link href={value.github} target='_blank'><GitHubIcon/></Link>
-            <Link href={`mailto:${value.email}`} target='_blank'><EmailIcon/></Link>
+        <div
+          key={index}
+          className="relative shadow-lg w-[400px] rounded-xl overflow-clip  h-[500px] flex-col justify-between mx-auto "
+        >
+          <div className="overflow-clip">
+            <Image
+              src={value.image}
+              blurDataURL={value.image}
+              alt="Profile Pic"
+              width={500}
+              height={500}
+            />
+          </div>
+          <div className=" flex justify-between p-4 items-center bg-white absolute bottom-0 z-10 w-full">
+            <span className=" text-[1.5rem]  ">{value.name}</span>
+            <div className="flex gap-4">
+              <Link href={value.linkedin} target="_blank">
+                <LinkedInIcon />
+              </Link>
+              <Link href={value.github} target="_blank">
+                <GitHubIcon />
+              </Link>
+              <Link href={`mailto:${value.email}`} target="_blank">
+                <EmailIcon />
+              </Link>
+              <Link href={value.website ? value.website : "#"} target="_blank">
+                <LanguageIcon />
+              </Link>
             </div>
-            </div>
+          </div>
         </div>
       ))}
     </div>
-  )
+  );
 }
